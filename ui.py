4dea8b4e04ae1b7311bf3fafecb20e96b93bd2a6
Dodoo Hellio <dorian.schnepp@gmail.com>
2024-08-21 17:58:25 +0200

    Error not fixed

--- ui.py
+++ ui.py
@@ -5,13 +5,14 @@ st.markdown(""" 💊 Drug & Smile 💊 """)
 
 direction = st.radio('Select a model', ('Random Forest', 'GNN', 'Logistic Regression'))
 
-st.write(direction)
+st.write(f'model selected ➤ {direction} ')
 
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
+"""
 uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 
 if uploaded_file is not None:
     data = pd.read_csv(uploaded_file)
-    st.write(data)
+    st.write(data) """
