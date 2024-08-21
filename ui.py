3dde4aee526add06286af6a3ee74b2483d804990
Dodoo Hellio <dorian.schnepp@gmail.com>
2024-08-21 17:43:10 +0200

    radio button

--- ui.py
+++ ui.py
@@ -1,3 +1,16 @@
 import streamlit as st
 
 st.markdown(""" 💊 Drug & Smile 💊 """)
+
+direction = st.radio('Select a model', ('Random Forest', 'GNN', 'Logistic Regression'))
+
+st.write(direction)
+
+if direction == 'top':
+    st.write('🔼')
+elif direction == 'right':
+    st.write('▶️')
+elif direction == 'bottom':
+    st.write('🔽')
+else:
+    st.write('◀️')
