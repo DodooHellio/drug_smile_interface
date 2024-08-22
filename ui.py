2aa57a52f2e33a5183125f6dd5988c77f5d5ca52
Dodoo Hellio <dorian.schnepp@gmail.com>
2024-08-22 11:15:08 +0200

    drag and drop added

--- ui.py
+++ ui.py
@@ -3,16 +3,22 @@ import pandas as pd
 
 st.markdown(""" 💊 Drug & Smile 💊 """)
 
-direction = st.radio('Select a model', ('Random Forest', 'GNN', 'Logistic Regression'))
-
-st.write(f'model selected ➤ {direction} ')
 
 
-st.set_option('deprecation.showfileUploaderEncoding', False)
+with st.form("my_form"):
+    st.write("Test your molecules")
+    model = st.radio('Select a model', ('Random Forest', 'GNN', 'Logistic Regression'))
 
-"""
-uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+    uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 
-if uploaded_file is not None:
-    data = pd.read_csv(uploaded_file)
-    st.write(data) """
+    if uploaded_file is not None:
+        data = pd.read_csv(uploaded_file)
+        st.write(data)
+
+    # Every form must have a submit button.
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        st.write("model selected ➤", model)
+        st.write("uploaded_file", uploaded_file)
+
+st.write("Results bellow")
